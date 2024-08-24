615e600b3a9d932b6f2b3412ef5840a09ddda4e8
Avinash Singh <avinashsingh888555@gmail.com>
2024-08-24 10:43:54 +0530

    setting upauth, working on signup

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,3 +1,4 @@
+import SingUpFormProvider from "@/components/forms/sign-up/form-provider";
 import React from "react";
 
 type Props = {};
@@ -6,13 +7,14 @@ const SignUp = (props: Props) => {
   return (
     <div className="flex-1 py-36 md:px-16 w-full">
       <div className="flex flex-col h-full gap-3">
-        {/* <SignUpFormProvider>
-          <div className="flex flex-col gap-3">
+        <SingUpFormProvider>
+          {/* <div className="flex flex-col gap-3">
             <RegistrationFormStep />
             <ButtonHandler />
           </div>
-          <HighLightBar />
-        </SignUpFormProvider> */}
+          <HighLightBar /> */}
+          <h1>hi</h1>
+        </SingUpFormProvider>
       </div>
     </div>
   );
